588ce1c3c46836683a9509f4d57eafeaf20399b9
Roychenlei <roychenfly@gmail.com>
2017-07-09 16:49:03 +0800

    candy issue

---
+++ Pmf.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+from thinkbayes import Pmf
+
+pmf = Pmf()
+pmf.Set('Bowl 1', 0.5)
+pmf.Set('Bowl 2', 0.5)
+
+
+pmf.Mult('Bowl 1', 0.75)
+pmf.Mult('Bowl 2', 0.5)
+
+
+
+pmf.Normalize() 
+
+print pmf.Prob('Bowl 1')
+
